afd143d4d9a37439bbe6fa9049be5b7b0504100d
natnaeltilahun <nataeltilahun@cbe.com.et>
2022-11-10 10:40:41 +0300

    Change fontawsome cdn and change plus icon

--- index.js
+++ index.js
@@ -57,7 +57,7 @@ function render() {
             <label for="">$${menu.price}</label>
           </div>
         </div>
-        <i class="fa-regular fa-circle-plus" data-id = "${menu.id}"></i>
+        <i class="fa-solid fa-circle-plus" data-id = "${menu.id}"></i>
       </div>
     `;
 
